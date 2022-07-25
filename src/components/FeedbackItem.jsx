4319b1ff3f073c912593cbdcdd725daaef7657c2
Ricardo Nogueira <ricnogueira01@gmail.com>
2022-07-25 19:47:03 +0100

    add global state with useContext, moving functions to context

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import Card from './Card';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackItem({ item: { id, rating, text }, handleDelete }) {
+function FeedbackItem({ item: { id, rating, text } }) {
+	const { deleteFeedback } = useContext(FeedbackContext);
 	return (
 		<Card>
 			<div className='num-display'>{rating}</div>
 			<button
 				onClick={() => {
-					handleDelete(id);
+					deleteFeedback(id);
 				}}
 				className='close'>
 				<FaTimes color='purple' />
