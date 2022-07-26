708585501e974076d0d579ffc65601be9ab45c6f
Ricardo Nogueira <ricnogueira01@gmail.com>
2022-07-26 16:26:53 +0100

    change state of selected item in RatingSelect

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -10,6 +10,7 @@ function RatingSelect({ select }) {
 	const [selected, setSelected] = useState(10);
 
 	const handleChange = (e) => {
+		setSelected(+e.currentTarget.value);
 		select(+e.currentTarget.value);
 	};
 
