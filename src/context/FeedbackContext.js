5a58cc7e9d408f3d0385ee649e22151fbd16c8c3
Ricardo Nogueira <ricnogueira01@gmail.com>
2022-07-26 15:38:56 +0100

    added proxy, update & delete from json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
@@ -17,7 +16,7 @@ export const FeedbackProvider = ({ children }) => {
 
 	//fetch feedback
 	const fetchFeedback = async () => {
-		const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc');
+		const response = await fetch('/feedback?_sort=id&_order=desc');
 		const data = await response.json();
 		setFeedback(data);
 		setIsLoading(false);
@@ -30,19 +29,32 @@ export const FeedbackProvider = ({ children }) => {
 		});
 	};
 
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
+	const addFeedback = async (newFeedback) => {
+		const response = await fetch('/feedback', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(newFeedback),
+		});
+		const data = await response.json();
+		setFeedback([data, ...feedback]);
 	};
 
-	const deleteFeedback = (id) => {
+	const deleteFeedback = async (id) => {
 		if (window.confirm(`Are you sure you want to delete the item with the id: ${id} `)) {
+			await fetch(`/feedback/${id}`, { method: 'DELETE' });
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
 
-	const updateFeedback = (id, updatedItem) => {
-		setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updatedItem } : item)));
+	const updateFeedback = async (id, updatedItem) => {
+		const response = await fetch(`/feedback/${id}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(updatedItem),
+		});
+
+		const data = await response.json();
+		setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...data } : item)));
 	};
 
 	return (
